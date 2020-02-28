ab2a3761b7cb33044e3836033866c047b041f6e6
RishitAghera <rishit4879@gmail.com>
2020-02-28 14:54:35 +0530

    index-card

--- gymcard/models.py
+++ gymcard/models.py
@@ -2,5 +2,3 @@ from django.db import models
 from accounts.models import User
 # Create your models here.
 
-class menmber(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
--- gymcard/views.py
+++ gymcard/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from accounts.models import Gym
-from django.views.generic import ListView
+from django.views import View
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
-class GymListView(ListView):
-    model = Gym
-    template_name = 'accounts/card.html'
-    
-    
+def GymListView(request,cat):
+
+    queryset=Gym.objects.all().filter(category=cat)
+    return render(request,'gymcard/card.html',{'queryset':queryset})
+
+class AllGym(ListView):
+    modela=Gym
+    template_name = 'gymcard/card.html'
+    context_object_name = 'queryset'
