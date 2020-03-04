b8b5f60c36aa491df6a0096532235897f258a289
RishitAghera <rishit4879@gmail.com>
2020-03-04 15:52:02 +0530

    card detail

--- gymcard/urls.py
+++ gymcard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # 'gymcard/'
     path('card/<str:cat>',views.GymListView,name='card'),
     path('card/all/',views.AllGym.as_view(),name='card-all'),
-    path('card/membership',views.detailCard,name='mem-detail'),
+    path('card/gym/',views.detailCard,name='gym-detail'),
+    path('card/membership/',views.detailCard,name='mem-detail'),
 ]
--- gymcard/views.py
+++ gymcard/views.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta, datetime
+
 from django.shortcuts import render
 from accounts.models import Gym
 from django.views import View
@@ -17,7 +19,26 @@ class AllGym(ListView):
     context_object_name = 'queryset'
 
 def detailCard(request):
-    membership=Membership.objects.all().get(user=request.user)
-    gym=Gym.objects.all().get(user=request.user)
+    day=''
     print(request.path)
-    return render(request,'gymcard/detailcard.html',{'gym':gym,'membership':membership})
+    if request.path == '/gymcard/card/membership/':
+        try:
+            membership=Membership.objects.all().get(user=request.user)
+            print(datetime.now())
+            left = membership.end_date - datetime.now().date()
+            day = str(left.days)
+            print(day)
+            gym = ''
+        except:
+            gym=''
+            membership=''
+
+    if request.path == '/gymcard/card/gym/':
+        try:
+            gym=Gym.objects.all().get(user=request.user)
+            membership = ''
+        except:
+            membership=''
+            gym = ''
+
+    return render(request,'gymcard/detailcard.html',{'membership':membership,'gym':gym,'day':day})
